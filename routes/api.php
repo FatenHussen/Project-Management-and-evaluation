096a90c0935085d3c25c17061466b5ab20e0353b
fatenhussen <155237524+FatenHussen@users.noreply.github.com>
2024-04-09 22:00:41 +0300

    api changes

--- routes/api.php
+++ routes/api.php
@@ -138,4 +138,4 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
-});
+}); 

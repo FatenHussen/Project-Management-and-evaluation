31615c32e68a3c1a5535a250b40d27fdd14cacd9
AbdallahZagh <f2002.a.z@gmail.com>
2024-05-21 22:53:21 +0300

    second

--- src/Pages/Weekly.js
+++ src/Pages/Weekly.js
@@ -13,37 +13,111 @@ const Weekly = () => {
     </div>
     <div className='w-[100%] h-fit flex justify-center items-center flex-col' dir="rtl">
       <p className='w-[90%] text-3xl font-bold text-[#27374d] my-5'>المتابعة الأسبوعية</p>
-      <div className='w-[90%]  border-2 border-b-0 border-[#27374d] text-[#27374d] mt-16'>
-            <div className='w-[30%] h-10 border-l-2 border-[#27374d] text-[#27374d]'>
+      <div className='w-[98%] bg-[#9fb0c9] border-2 border-b-0 border-[#27374d] text-[#27374d] mt-16 flex justify-center items-center'>
+            <div className='w-[20%] h-[100%] p-2 border-l-2 border-[#27374d] text-[#27374d] flex justify-center items-center'>
                 <select className='bg-transparent outline-none w-[75%]'>
                     <option>sadas</option>
                     <option>sadas</option>
                 </select> 
               </div>
-              <p className="w-[20%] h-10 border-l-2 border-[#27374d] text-[#27374d]">اسم المشروع</p>
-              <div className='w-[50%]'>
+              <p className="w-[18%] h-[100%] border-l-2 border-[#27374d] text-[#27374d] p-2">اسم المشروع</p>
+              <div className='w-[62%]'>
                 <input 
                   type="text"
-className='w-[100%] h-10 outline-none bg-transparent' 
+className='w-[100%] h-[100%] p-2 outline-none bg-transparent' 
                   name="projectName"   
                   disabled
                 />
               </div>
               </div>
-      <table className="w-[90%]  border-2 border-[#27374d] text-[#27374d] mb-10" border="">
-            <tr>
-                <td className='w-[10%] text-center'>الحضور</td>
-                <td className='w-[10%] text-center'>Testing</td>
-                <td className='w-[10%] text-center'>Code</td>
-                <td className='w-[10%] text-center'>Design</td>
-                <td className='w-[10%] text-center'>Presentation</td>
-                <td className='w-[10%] text-center'>Report</td>
-                <td className='w-[10%] text-center'>Github</td>
-                <td className='w-[10%] text-center'>reference_study</td>
-                <td className='w-[10%] text-center'>analytical_study</td>
-                <td className='w-[10%] text-center'>results</td>
+      <table className="w-[98%]  border-2 border-[#27374d] text-[#27374d] mb-10" border="2">
+            <tr className='border-b-2 border-[#27374d]'>
+                <td className='w-[20%] h-10 text-center border-l-2 border-[#27374d]'>الحضور</td>
+                <td className='w-[9%] h-10 text-center border-l-2 border-[#27374d]'>Testing</td>
+                <td className='w-[9%] h-10 text-center border-l-2 border-[#27374d]'>Code</td>
+                <td className='w-[9%] h-10 text-center border-l-2 border-[#27374d]'>Design</td>
+                <td className='w-[9%] h-10 text-center border-l-2 border-[#27374d]'>Presentation</td>
+                <td className='w-[9%] h-10 text-center border-l-2 border-[#27374d]'>Report</td>
+                <td className='w-[9%] h-10 text-center border-l-2 border-[#27374d]'>Github</td>
+                <td className='w-[9%] h-10 text-center border-l-2 border-[#27374d]'>reference_study</td>
+                <td className='w-[9%] h-10 text-center border-l-2 border-[#27374d]'>analytical_study</td>
+                <td className='w-[9%] h-10 text-center'>results</td>
+            </tr>
+            <tr className='border-b-2 border-[#27374d]'>
+                <td className='w-[20%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center'></td>
+            </tr>
+            <tr className='border-b-2 border-[#27374d]'>
+                <td className='w-[20%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center'></td>
+            </tr>
+            <tr className='border-b-2 border-[#27374d]'>
+                <td className='w-[20%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center'></td>
+            </tr>
+            <tr className='border-b-2 border-[#27374d]'>
+                <td className='w-[20%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center'></td>
+            </tr>
+            <tr className='border-b-2 border-[#27374d]'>
+                <td className='w-[20%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center border-l-2 border-[#27374d]'></td>
+                <td className='w-[9%] h-14 text-center'></td>
+            </tr>
+            <tr className='border-b-2 border-[#27374d]'>
+                <td className='w-[20%] h-10 text-center border-l-2 border-[#27374d]'>المجموع</td>
+                <td className='w-[9%] h-10 text-center'></td>
+            </tr>
+            <tr className='border-b-2 border-[#27374d]'>
+                <td className='w-[20%] h-10 text-center border-l-2 border-[#27374d]'>التقييم</td>
+                <td className='w-[9%] h-10 text-center'></td>
             </tr>
           </table>
+          <div className='w-[90%] h-fit mb-5 flex justify-center items-center flex-col text-[#27374d]'>
+            <p className='w-[100%] text-[#27374d] font-bold text-2xl'>الملاحظات</p>
+            <input type='text' className='w-[90%] h-80 bg-transparent' disabled/>
+            <p className='w-[100%] text-[#27374d] font-bold text-2xl'>المهام</p>
+            <input type='text' className='w-[90%] h-80 bg-transparent' disabled/>
+          </div>
       </div>
       </div>
   )
